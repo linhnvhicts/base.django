a93332e878df2360004a63a8457c36a6fb7b5c8d
Trung Nguyen <nttrung143@gmail.com>
2018-06-24 14:06:31 +0700

    add unittest

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class UserModelTest(TestCase):
+
+    def test_string_representation(self):
+        username="trungnt"
+        email="trungnt@socicom.vn"
+        user = User(username=username, email=email)
+        self.assertEqual(str(user), user.username)
