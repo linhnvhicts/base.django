b6cdcea043eb5110de7d883c89a0a134653b6dad
Trung Nguyen <nttrung143@gmail.com>
2018-07-28 11:12:29 +0700

    test

--- base/settings/circleci.py
+++ base/settings/circleci.py
@@ -3,3 +3,10 @@ from .defaults import *
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+def show_toolbar(request):
+    return False
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+}
