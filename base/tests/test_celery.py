0cd52492f25a431c1947445d0daed07214bb73be
Trung Nguyen <nttrung143@gmail.com>
2020-11-16 15:37:34 +0700

    disable celery

--- base/tests/test_celery.py
+++ base/tests/test_celery.py
@@ -13,9 +13,9 @@ class CeleryTest(TestCase):
     def setUp(self):
         return None
 
-    def test_test(self):
-        str = 'hello123'
-        task = test.s(str=str).delay()
-        result = task.get()
-        self.assertEqual(result, str)
-        self.assertEqual(task.status, 'SUCCESS')
+    # def test_test(self):
+    #     str = 'hello123'
+    #     task = test.s(str=str).delay()
+    #     result = task.get()
+    #     self.assertEqual(result, str)
+    #     self.assertEqual(task.status, 'SUCCESS')
