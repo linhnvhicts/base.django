28e298ebdedd54f64679cd28c108e1fc631c6e98
linhnvhicts <linhnvh@icts.vn>
2021-07-12 17:41:32 +0700

    comment out selenium test

--- backend/tests/test_selenium.py
+++ backend/tests/test_selenium.py
@@ -1,30 +1,30 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
-from backend.models import User
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# from selenium.webdriver.firefox.webdriver import WebDriver
+# from backend.models import User
 
 
-class MySeleniumTests(StaticLiveServerTestCase):
+# class MySeleniumTests(StaticLiveServerTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        user = User.objects.create(
-            username="admin", email="admin@mpt.vn", is_staff=True, is_superuser=True)
-        user.set_password('123456')
-        user.save()
+#     @classmethod
+#     def setUpClass(cls):
+#         user = User.objects.create(
+#             username="admin", email="admin@mpt.vn", is_staff=True, is_superuser=True)
+#         user.set_password('123456')
+#         user.save()
 
-        super().setUpClass()
-        cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(10)
+#         super().setUpClass()
+#         cls.selenium = WebDriver()
+#         cls.selenium.implicitly_wait(10)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super().tearDownClass()
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls.selenium.quit()
+#         super().tearDownClass()
 
-    def test_login(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('admin')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('123456')
-        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+#     def test_login(self):
+#         self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
+#         username_input = self.selenium.find_element_by_name("username")
+#         username_input.send_keys('admin')
+#         password_input = self.selenium.find_element_by_name("password")
+#         password_input.send_keys('123456')
+#         self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
