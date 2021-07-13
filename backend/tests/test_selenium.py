d2ed09b9efb05485b42fd9a9c8c62dc7b1926223
linhnvhicts <linhnvh@icts.vn>
2021-07-13 11:59:06 +0700

    update selenium test

--- backend/tests/test_selenium.py
+++ backend/tests/test_selenium.py
@@ -13,20 +13,18 @@ class MySeleniumTests(StaticLiveServerTestCase):
         user.save()
 
         super().setUpClass()
-        try:
-            WebDriver().implicitly_wait(10)
-        except Exception as e:
-            print(e)
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
 
     @classmethod
     def tearDownClass(cls):
-        WebDriver().quit()
+        cls.selenium.quit()
         super().tearDownClass()
 
     def test_login(self):
-        WebDriver().get('%s%s' % (self.live_server_url, '/admin/'))
-        username_input = WebDriver().find_element_by_name("username")
+        self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
+        username_input = self.selenium.find_element_by_name("username")
         username_input.send_keys('admin')
-        password_input = WebDriver().find_element_by_name("password")
+        password_input = self.selenium.find_element_by_name("password")
         password_input.send_keys('123456')
-        WebDriver().find_element_by_xpath('//input[@value="Log in"]').click()
+        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
